902f5991d6574d26fa2950350bf468855e85d2a6
Christian John jiménez Calvo <christianjjc@mail.com>
2024-10-03 17:32:16 -0500

    sect12-ep11-login-usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -14,29 +15,59 @@ export class AuthService extends PrismaClient implements OnModuleInit {
 
   async registerUser(registerUserDto: RegisterUserDto) {
     const { email, name, password } = registerUserDto;
-
     try {
       const user = await this.user.findUnique({
         where: { email },
       });
-
       if (user) {
         throw new RpcException({
           status: 400,
           message: 'User already exist!',
         });
       }
-
       const newUser = await this.user.create({
         data: {
           email: email,
           name: name,
-          password: password,
+          password: bcrypt.hashSync(password, 10),
         },
       });
-
+      const { password: __, ...rest } = newUser;
       return {
-        user: newUser,
+        user: rest,
+        token: 'ABC',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    try {
+      const user = await this.user.findUnique({
+        where: { email },
+      });
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid!',
+        });
+      }
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid!',
+        });
+      }
+      const { password: __, ...rest } = user;
+      return {
+        user: rest,
+        token: 'ABC',
       };
     } catch (error) {
       throw new RpcException({
